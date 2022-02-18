b7fed15b478815d91c693701e7bd3085f9f98a16
anqiubc <87057979+anqiubc@users.noreply.github.com>
2022-02-18 14:33:35 -0800

    Update get_history_astro_information.R

--- R/get_history_astro_information.R
+++ R/get_history_astro_information.R
@@ -10,12 +10,17 @@
 #' get_history_astro_information("London","2022-02-12")
 get_history_astro_information<- function(q,dt) {
   df <- data.frame()
+  # Define url and initialize df to store results
   base_url <- "http://api.weatherapi.com/v1/history.json"
+  # Get weather info via: url + api_key + location + aqi
   resp <- httr::GET(base_url, query = list(key=api_key(),q=q,dt=dt))
+  # Handle error if API did not return json
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
+  # Parse response to json
   parsed <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"), simplifyVector = FALSE)
+  # Handle error due to invalid api key or location
   if (httr::http_error(resp)) {
     stop(
       sprintf(
@@ -27,5 +32,6 @@ get_history_astro_information<- function(q,dt) {
       call. = FALSE
     )
   }
+  # Return the dataframe result
   return(as.data.frame(parsed[[2]]$forecastday[[1]]$astro))
 }
